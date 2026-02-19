cb3a24ef13c50ab9a7c081f9b7dbb3f045b06e14
Carlos Daniel Oliva Moreno <coliva@unab.edu.co>
2026-02-19 11:04:44 -0500

    Readme, Rutas y Controladores Basicos

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, '_invoke']);
+
+
+
+Route::get('/product', [ProductController::class, 'index']);
+
+Route::get('/product/create', [ProductController::class, 'create']);
+
+Route::get('/product/{producto}', [ProductController::class, 'show']);
