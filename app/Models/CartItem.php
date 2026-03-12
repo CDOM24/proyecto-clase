e63258be6d83bd2bfdcadb85bdc9f8542885c756
Carlos Oliva <coliva@unab.edu.co>
2026-03-12 00:17:17 -0500

    FactoriesArreglos

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
+    use HasFactory;
+
     protected $table = 'cart_items';
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'quantity'
+    ];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\Product;
+use App\Models\CartItem;
 
 
 class DatabaseSeeder extends Seeder
@@ -21,7 +23,9 @@ class DatabaseSeeder extends Seeder
             
         ]);*/
 
-        Category::factory(1000)->create();
-        Product::factory(1000)->create();
+        User::factory(100)->create();
+        Category::factory(100)->create();
+        Product::factory(200)->create();
+        CartItem::factory(300)->create();
     }
 }
